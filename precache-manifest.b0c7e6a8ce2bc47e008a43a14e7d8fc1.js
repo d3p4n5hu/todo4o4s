0c3f526f11777ae471ecf53a8d30fe23994ab4f9
d3p4n5hu <depanshudhiman@gmail.com>
2019-12-31 20:20:22 +0530

    Updates

---
+++ precache-manifest.b0c7e6a8ce2bc47e008a43a14e7d8fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727245437de25c7653aa08b573310a4c",
+    "url": "/todo4o4s/index.html"
+  },
+  {
+    "revision": "88c38218adfcb996f01c",
+    "url": "/todo4o4s/static/css/main.216728e0.chunk.css"
+  },
+  {
+    "revision": "b646c33bd93c69bc8411",
+    "url": "/todo4o4s/static/js/2.a020db34.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo4o4s/static/js/2.a020db34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88c38218adfcb996f01c",
+    "url": "/todo4o4s/static/js/main.019adb6b.chunk.js"
+  },
+  {
+    "revision": "da85e9eac7a8848fd86c",
+    "url": "/todo4o4s/static/js/runtime-main.3944971d.js"
+  }
+]);
